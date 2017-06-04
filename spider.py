77590257b11681c4af37a19f085e7f0728dad908
jwma <mjw@majiawei.com>
2017-06-04 22:56:47 +0800

    将爬取回来的新闻数据保存到本地对应日期的目录

--- spider.py
+++ spider.py
@@ -1,25 +1,31 @@
 # coding=utf-8
-from urllib.request import urlopen
+from urllib.request import urlopen, urlretrieve
 import time
 import json
 import sys
+import os
 
 url = 'https://h5.newaircloud.com/api/getLayouts?sid=aomen&cid=10038&date='
 full_list_response = urlopen(url)
 list_data = json.loads(full_list_response.read().decode('utf8'))
 full_list_response.close()
 
+date = list_data.get('date')
+new_tmp_data_path = './tmp_data/%s' % date
+if not os.path.exists(new_tmp_data_path):
+    os.mkdir(new_tmp_data_path)
+
 layouts = list_data.get('layouts')
 
 if layouts is None:
-    print('返回的数据没有版面数据，请手动检查接口数据是否正确。')
+    print('返回的数据没有版面数据，请手动检查接口数据是否正确')
     sys.exit()
 
 for layout in layouts:
     layout_name = layout.get('name')
 
     if layout_name is None:
-        print('版面名称不存在，跳过。')
+        print('版面名称不存在，跳过')
         continue
 
     layout_date = layout.get('date')
@@ -36,17 +42,12 @@ for layout in layouts:
         news_pic = news.get('pic1')
         news_data_url = news.get('curl')
 
-        news_data_response = urlopen(news_data_url)
-        news_data_str = news_data_response.read().decode('utf8')
-        news_data_response.close()
-
-        print('%s | %s' % (layout_name, layout_date))
+        print('当前：%s | %s' % (layout_name, layout_date))
         print(news_data_url)
-        news_data = eval(news_data_str.strip('var gArticleJson = '),
-                         type('Dummy', (dict,), dict(__getitem__=lambda s, n: n))())
-        print(news_data)
-        print('')
 
-        time.sleep(0.3)
+        urlretrieve(news_data_url, '%s/%s.js' % (new_tmp_data_path, news_id))
+        time.sleep(0.1)
 
-    time.sleep(1)
+    time.sleep(0.3)
+
+print('数据抓取完成')
